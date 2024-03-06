58dc53ae7e9b5f05edff5e5225557c367bd332d7
Kushla95 <das.kushal95@gmail.com>
2024-03-06 16:52:45 +0530

    Added navbar, footer and enhanced more components

--- src/components/room/EditRoom.jsx
+++ src/components/room/EditRoom.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getRoomById, updateRoom } from "../utils/ApiFunctions";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const EditRoom = () => {
   const [room, setRoom] = useState({
-    photo: null,
+    photo: "",
     roomType: "",
     roomPrice: "",
   });
@@ -26,6 +26,19 @@ const EditRoom = () => {
     setRoom({ ...room, [name]: value });
   };
 
+  useEffect(() => {
+    const fetchRoom = async () => {
+      try {
+        const roomData = await getRoomById(roomId);
+        setRoom(roomData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchRoom();
+  }, [roomId]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -46,8 +59,87 @@ const EditRoom = () => {
     }
   };
   return (
-    <div>
-      <h2>EditRoom</h2>
+    <div className="container mt-5 mb-5">
+      <h3 className="text-center mb-5 mt-5">Edit Room</h3>
+      <div className="row justify-content-center">
+        <div className="col-md-8 col-lg-6">
+          {successMessage && (
+            <div className="alert alert-success" role="alert">
+              {successMessage}
+            </div>
+          )}
+
+          {errorMessage && (
+            <div className="alert alert-danger" role="alert">
+              {errorMessage}
+            </div>
+          )}
+
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="roomType" className="form-label hotel-color">
+                Room Type
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="roomType"
+                name="roomType"
+                value={room.roomType}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="roomPrice" className="form-label">
+                Room Price
+              </label>
+              <input
+                className="form-control"
+                required
+                id="roomPrice"
+                type="number"
+                name="roomPrice"
+                value={room.roomPrice}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="photo" className="form-label">
+                Photo
+              </label>
+              <input
+                required
+                id="photo"
+                name="photo"
+                type="file"
+                className="form-control"
+                onChange={handleImageChange}
+              />
+              {imagePreview && (
+                <img
+                  src={`data:image/jpeg;base64,${imagePreview}`}
+                  alt="Room preview"
+                  style={{ maxWidth: "400px", maxHeight: "400px" }}
+                  className="mt-3"
+                />
+              )}
+            </div>
+            <div className="d-grid gap-2 d-md-flex mt-2">
+              <Link
+                to={"/existing-rooms"}
+                className="btn btn-outline-info ml-5"
+              >
+                back
+              </Link>
+              <button type="submit" className="btn btn-outline-primary ml-5">
+                Edit Room
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
